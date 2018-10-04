403b381102661841f9190dab6d950d39f9e2a855
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2018-10-04 17:20:18 +0200

    [RD-187] Test exception thrown on invalid input

--- src/test/java/ValidationTest.java
+++ src/test/java/ValidationTest.java
@@ -8,11 +8,25 @@ import org.junit.jupiter.api.Test;
 
 import javax.xml.stream.XMLStreamException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class ValidationTest {
 
-    @Test
-    public void testMyXML() throws XMLStreamException {
-        XMLValidatorUsingTreeGrammars validator = new XMLValidatorUsingTreeGrammars();
-        validator.parse("<root><markup>tekst</markup></root>");
+  @Test
+  public void testXMLParses() throws XMLStreamException {
+    XMLValidatorUsingTreeGrammars validator = new XMLValidatorUsingTreeGrammars();
+    validator.parse("<root><markup>tekst</markup></root>");
+  }
+
+  @Test
+  public void testXMLDoesNotParse() throws XMLStreamException {
+    XMLValidatorUsingTreeGrammars validator = new XMLValidatorUsingTreeGrammars();
+    try {
+      validator.parse("<root><markup>tekst and <b>more</b> markup</markup></root>");
+      fail("Expected an exception");
+    } catch (Exception e) {
+      assertThat(e).hasMessage("No transition rule found! Current state: null -> [b]");
     }
+  }
 }
